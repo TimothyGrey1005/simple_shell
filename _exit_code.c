5b08452851c43e90dbac5475fac3bf9b74054fb7
OmphileGopane <omphilegopane913@gmail.com>
2023-07-22 06:43:27 -0700

    _exit_code.c

---
+++ _exit_code.c
@@ -0,0 +1,74 @@
+ #include "shell.h"
+
+/**
+ **_strncpy - Copies string
+ *@dest: The string is to be copied to this destination
+ *@src: String source
+ *@n: the number of characters that are to be copied
+ *Return: String concatenation
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - Two string concatenations
+ *@dest: First string
+ *@src: Second string
+ *@n: Number of bytes used maximally
+ *Return: String concatenation
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - Finds a character in string
+*@s: String to parse
+ *@c: Character to search
+ *Return: A value s, is returned to a pointer to s which is the area memory
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
