34c2cc9beb38446e9abf3b10793b8d3e243eb635
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-07-25 07:02:41 -0700

    Updated

--- vars.c
+++ vars.c
@@ -1,58 +1,64 @@
 #include "shell.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * is_chain - a funct to see current char
+ * is_chain - a func to checks chain del
  * @info: param
- * @buf: buffer car
- * @p: curr poss addre
+ * @buf: c buffer
+ * @p: address
  * Return: results
  */
+
 int is_chain(info_t *info, char *buf, size_t *p)
 {
-	size_t j = *p;
+	size_t s = *p;
 
-	if (buf[j] == '|' && buf[j + 1] == '|')
+	if (buf[s] == '|' && buf[s + 1] == '|')
 	{
-		buf[j] = 0;
-		j++;
+		buf[s] = 0;
+		s++;
 		info->cmd_buf_type = CMD_OR;
 	}
-	else if (buf[j] == '&' && buf[j + 1] == '&')
+
+	else if (buf[s] == '&' && buf[s + 1] == '&')
 	{
-		buf[j] = 0;
-		j++;
+		buf[s] = 0;
+		s++;
 		info->cmd_buf_type = CMD_AND;
 	}
-	else if (buf[j] == ';') /* found end of this command */
+
+	else if (buf[s] == ';')
 	{
-		buf[j] = 0; /* replace semicolon with null */
+		buf[s] = 0;
 		info->cmd_buf_type = CMD_CHAIN;
 	}
 	else
 		return (0);
-	*p = j;
+	*p = s;
 	return (1);
 }
 
 /**
- * check_chain - a funct to see if carry on
- * @info: param
- * @buf: buffer for charactr
- * @p: buffer pos
- * @i: buffer start pos
- * @len: buffer len
- * Return: nothing
+ * check_chain - a func to test chaining
+ * @info: param struct
+ * @buf: char buff
+ * @p: buff pos address
+ * @i: start pos
+ * @len: buff len
+ * Return: none
  */
+
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 {
-	size_t j = *p;
+	size_t s = *p;
 
 	if (info->cmd_buf_type == CMD_AND)
 	{
 		if (info->status)
 		{
 			buf[i] = 0;
-			j = len;
+			s = len;
 		}
 	}
 	if (info->cmd_buf_type == CMD_OR)
@@ -60,90 +66,108 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 		if (!info->status)
 		{
 			buf[i] = 0;
-			j = len;
+			s = len;
 		}
 	}
 
-	*p = j;
+	*p = s;
 }
 
 /**
- * replace_alias - a funct to replace a name in str
+ * replace_alias - a function that switches
  * @info: param
  * Return: results
  */
+
 int replace_alias(info_t *info)
 {
-	int i;
+	int s;
 	list_t *node;
-	char *p;
+	char *c;
 
-	for (i = 0; i < 10; i++)
+	for (s = 0; s < 10; s++)
 	{
 		node = node_starts_with(info->alias, info->argv[0], '=');
 		if (!node)
+		{
 			return (0);
+		}
 		free(info->argv[0]);
-		p = _strchr(node->str, '=');
-		if (!p)
+		c = strchr(node->string, '=');
+
+		if (!c)
+		{
 			return (0);
-		p = _strdup(p + 1);
-		if (!p)
+		}
+
+		c = _strdup(c + 1);
+
+		if (!c)
+		{
 			return (0);
-		info->argv[0] = p;
+		}
+
+		info->argv[0] = c;
 	}
 	return (1);
 }
 
 /**
- * replace_string - a function to replace str
- * @old: old string
- * @new: new str
- * Return: string
- */
-int replace_string(char **old, char *new)
-{
-        free(*old);
-        *old = new;
-        return (1);
-}
-
-/**
- * replace_vars - a funct to replace variables in string
+ * replace_vars - a func to switch variables
  * @info: param
- * Return: results
+ * Return: return
  */
+
 int replace_vars(info_t *info)
 {
-	int i = 0;
+	int s = 0;
 	list_t *node;
 
-	for (i = 0; info->argv[i]; i++)
+	for (s = 0; info->argv[s]; s++)
 	{
-		if (info->argv[i][0] != '$' || !info->argv[i][1])
-			continue;
-
-		if (!_strcmp(info->argv[i], "$?"))
+		if (info->argv[s][0] != '$' || !info->argv[s][1])
 		{
-			replace_string(&(info->argv[i]),
+			continue;
+		}
+
+		if (!_strcmp(info->argv[s], "$?"))
+		{
+			replace_string(&(info->argv[s]),
 					_strdup(convert_number(info->status, 10, 0)));
 			continue;
 		}
-		if (!_strcmp(info->argv[i], "$$"))
+
+		if (!_strcmp(info->argv[s], "$$"))
 		{
-			replace_string(&(info->argv[i]),
+			replace_string(&(info->argv[s]),
 					_strdup(convert_number(getpid(), 10, 0)));
 			continue;
 		}
-		node = node_starts_with(info->env, &info->argv[i][1], '=');
+
+		node = node_starts_with(info->env, &info->argv[s][1], '=');
+
 		if (node)
 		{
-			replace_string(&(info->argv[i]),
-					_strdup(_strchr(node->str, '=') + 1));
+			replace_string(&(info->argv[s]),
+					_strdup(_strchr(node->string, '=') + 1));
 			continue;
 		}
-		replace_string(&info->argv[i], _strdup(""));
 
+		replace_string(&info->argv[s], _strdup(""));
 	}
+
 	return (0);
+}
+
+/**
+ * replace_string - a func to check that switches strings
+ * @old1: addr
+ * @new1: str
+ * Return: results
+ */
+int replace_string(char **old1, char *new1)
+{
+	free(*old1);
+	*old1 = new1;
+	return (1);
 }
